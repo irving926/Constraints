91f52bdd227f69b0c963d99d2bd1defde62bc934
irving926 <irving926@gmail.com>
2016-07-18 13:49:47 +0800

    Initial Commit

---
+++ hw5_Constraints_3ImageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw5_Constraints_3ImageView
+//
+//  Created by 周 修儀 on 2016/7/18.
+//  Copyright © 2016年 周 修儀. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
